139d5c813e7b09ca0236c38d2ee89ccdb865fc97
abutoku <ystkz2021@gmail.com>
2022-04-27 16:00:51 +0900

    Merge branch 'main' of github.com:abutoku/divee_alpha_v2

--- resources/views/infomation/logo.blade.php
+++ resources/views/infomation/logo.blade.php
@@ -16,7 +16,7 @@
 
         <div class="p-12">
             {{-- 入力フォーム --}}
-            <form action="{{ route('profile.update',$infomation->id) }}" method="post" enctype="multipart/form-data">
+            <form action="{{ route('infomation.updatelogo',$infomation->id) }}" method="post" enctype="multipart/form-data">
                 @method('patch')
                 @csrf
                 <div class="flex justify-center">
@@ -24,7 +24,7 @@
                     <img src="{{ Storage::url($infomation->logo_image) }}" id="demo_img" class="rounded-full h-48 w-48 object-cover mb-12 bg-white" >
                 </div>
                     {{-- ファイル選択欄 --}}
-                    <input type="file" name="profile_image" id="profile_image" class="mb-6"><br>
+                    <input type="file" name="logo" id="logo_image" class="mb-6"><br>
                     {{-- 変更ボタン --}}
                     <x-button>変更</x-button>
             </form>
