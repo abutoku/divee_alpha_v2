083d17b82803e019c2c1a93b75c1dd73fd8119df
abutoku <ystkz2021@gmail.com>
2022-04-30 13:41:57 +0900

    update styles

--- resources/views/book/show.blade.php
+++ resources/views/book/show.blade.php
@@ -51,25 +51,27 @@
 
             <div class="w-1/2 sm:w-1/3 flex flex-col items-center">
                 {{-- 生物名 --}}
-                <h1 class="mt-8  md:text-3xl font-bold">{{ $book->fish_name }}</h1>
+                <h1 class="mt-8  text-lg md:text-3xl font-bold">{{ $book->fish_name }}</h1>
                 @if($book->order)
                     <div class="flex">
-                        <p class="mr-6">{{ $book->order }}</p>
-                        <p>{{ $book->family }}</p>
+                        <p class="text-xs lg:text-base mr-6">{{ $book->order }}</p>
+                        <p class="text-xs lg:text-base">{{ $book->family }}</p>
                     </div>
                 @endif
-                {{-- 説明文 --}}
-                @if($book->info)
-                    <div class="mt-4 ml-2 p-4 rounded-lg w-11/12 h-56 border bg-slate-50">
-                        {!! nl2br(e($book->info)) !!}
-                    </div>
-                @else
-                    <div class="mt-4 ml-2 p-4 rounded-lg w-11/12 h-56 sm:h-72 border bg-slate-50"><a href="{{ route('book.memo',$book->id ) }}" class="flex"><svg class="h-5 w-5 text-gray-500" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
-                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
-                        <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
-                    </svg>Memo</a></div>
-                @endif
+                <section class="hidden lg:block w-11/12">
+                    {{-- 説明文 --}}
+                    @if($book->info)
+                        <div class="mt-4 ml-2 p-4 rounded-lg h-56 border bg-slate-50">
+                            {!! nl2br(e($book->info)) !!}
+                        </div>
+                    @else
+                        <div class="mt-4 ml-2 p-4 rounded-lg  h-56 sm:h-72 border bg-slate-50"><a href="{{ route('book.memo',$book->id ) }}" class="flex"><svg class="h-5 w-5 text-gray-500" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
+                            <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
+                        </svg>Memo</a></div>
+                    @endif
+                </section>
             </div>
         </div>
     </section>
@@ -92,7 +94,7 @@
     </section> --}}
 
     {{-- 地図選択 --}}
-    <section class="flex flex-col w-[300px]">
+    <section class="flex flex-col w-[300px] mt-12">
 
             <select id="map" name="site_id" class="rounded-lg border-2 border-divenavy my-2">
                 <option>
@@ -111,7 +113,7 @@
     <div class="md:flex md:justify-center">
 
         <div class="mt-10 flex justify-center">
-            <canvas id="canvas" width="400" height="300" style="border:1px solid #000;" class="mt-4"></canvas>
+            <canvas id="canvas" width="400" height="300" style="border:1px solid #000; background-color:white;" class="mt-4"></canvas>
         </div>
 
         <section class="mt-12 lg:w-1/2">
