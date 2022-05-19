8bd39c45e898d479cb6eb88cd93aefdc33df4e3d
abutoku <ystkz2021@gmail.com>
2022-05-19 12:48:12 +0900

    test update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -29,10 +29,12 @@
                 <!-- 生物ログ-->
                 <a href="{{ route('log.index') }}"
                     class=" rounded-lg drop-shadow-md w-11/12  h-32 lg:h-40  mb-6 flex justify-center items-center bg-cover bg-center"
-                    style="background-image: url('log.jpg');">
+                    style="background-image: url('/log.jpg');">
                     <span class="text-white text-xl font-bold bg-black bg-opacity-50 rounded-lg py-2 px-6">生物ログ</span>
                 </a>
             </div>
+
+            <img src="{{ Storage::url('uploads/tide.jpg') }}" class="w-full h-80 object-cover bg-white rounded-lg shadow-xl">
 
             <div class="flex justify-around w-full">
                 <!-- 陸の情報 -->
@@ -50,7 +52,7 @@
                 <!-- Map -->
                 <a href="{{ route('map.site') }}"
                     class="rounded-lg drop-shadow-md w-11/12 h-32 lg:h-40 mb-6 flex justify-center items-center bg-cover bg-center"
-                    style="background-image: url('storage/uploads/map.jpg');">
+                    style="background-image: url('/storage/uploads/map.jpg');">
                     <span class="text-white text-xl font-bold bg-black bg-opacity-50 rounded-lg py-2 px-6">全国の生物Map</span>
                 </a>
             </div>
@@ -60,7 +62,7 @@
                 <!-- 陸の情報 -->
                 <a href="{{ route('map.post') }}"
                     class="rounded-lg drop-shadow-md w-11/12 h-32 lg:h-40 mb-6 flex justify-center items-center bg-cover bg-center"
-                    style="background-image: url('storage/uploads/aft.jpg');">
+                    style="background-image: url('./storage/uploads/aft.jpg');">
                     <span class="text-white text-xl font-bold bg-black bg-opacity-50 rounded-lg py-2 px-6">観光スポット</span>
                 </a>
             </div>
